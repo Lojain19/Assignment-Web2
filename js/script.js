16aac9e66a14bd9d65cf344738411d3e07c9e784
Lojain19 <dyfallhalrawashdh@gmail.com>
2026-08-08 22:41:20 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
-```javascript
+
 const menuItems = document.querySelectorAll(".menu-item");
 
 menuItems.forEach(function(item) {
-
     item.addEventListener("click", function() {
 
         const fileName = item.getAttribute("data-file");
 
         fetch("json/" + fileName)
             .then(function(response) {
-                if (!response.ok) {
-                    throw new Error("JSON file not found");
-                }
-
                 return response.json();
             })
             .then(function(data) {
@@ -20,32 +15,23 @@ menuItems.forEach(function(item) {
                 let output = "";
 
                 data.forEach(function(food) {
-
-                    output += `
-                        <div class="food-item">
-                            <h2>${food.name}</h2>
-                            <p>${food.description}</p>
-                        </div>
-                    `;
-
+                    output += '<div class="food-item">';
+                    output += '<h2>' + food.name + '</h2>';
+                    output += '<p>' + food.description + '</p>';
+                    output += '</div>';
                 });
 
-                document.body.innerHTML = `
-                    <h1>Menu Details</h1>
-                    <div class="food-container">
-                        ${output}
-                    </div>
-                `;
+                document.body.innerHTML =
+                    '<h1>Menu Details</h1>' +
+                    '<div class="food-container">' +
+                    output +
+                    '</div>';
 
             })
             .catch(function(error) {
-
-                console.error(error);
+                console.log(error);
                 alert("There is a problem loading the JSON file.");
-
             });
 
     });
-
 });
-```
