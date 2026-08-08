ac53d5e71d1b1d9237160e80e2903af2d543347d
Lojain19 <dyfallhalrawashdh@gmail.com>
2026-08-08 22:18:13 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+```javascript id="wz8n3m"
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(function(item) {
+
+    item.addEventListener("click", function() {
+
+        const fileName = item.getAttribute("data-file");
+
+        fetch("json/" + fileName)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+
+                let output = "";
+
+                data.forEach(function(food) {
+
+                    output += `
+                        <div class="food-item">
+                            <h2>${food.name}</h2>
+                            <p>${food.description}</p>
+                        </div>
+                    `;
+                });
+
+                document.body.innerHTML = `
+                    <h1>Menu Details</h1>
+                    <div class="food-container">
+                        ${output}
+                    </div>
+                `;
+
+            })
+            .catch(function(error) {
+
+                console.log(error);
+
+                alert("There is a problem loading the JSON file.");
+
+            });
+
+    });
+
+});
+```
